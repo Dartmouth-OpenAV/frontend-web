9d70208ac7425e734ec605d319916b099e56d438
Jane Reynolds <jane.e.reynolds@dartmouth.edu>
2025-07-21 17:13:43 -0400

    Stubbing out Zoom join and leave modals/forms

--- source/optional_modules/zoom_room/index.js
+++ source/optional_modules/zoom_room/index.js
@@ -1,6 +1,7 @@
 /* Global variables */
 import { globals } from '../../js/globals.js';
-import { attachSharedModalListeners } from '../../js/modals.js';
+import { updateStatus } from '../../js/main.js';
+import { attachSharedModalListeners, openModal } from '../../js/modals.js';
 import banners from './components/zoom_banners.html'
 import joinManualModal from './components/join_manual_modal.html'
 import joinSuggestedModal from './components/join_suggested_modal.html'
@@ -16,32 +17,186 @@ const abandonedZoomWaitTime = 7200000; // milliseconds (2hs)
 const REFRESH_WAIT = 5000 ;
 const MAX_TRIES = 5;
 let guiInitiationTries = 0;
-let zoomStatus;
+let zoomData;
 
-function getZoomStatus() {
+function joinZoomMeeting(meetingId, password, callback=null) {
+  // stop the getZoomData loop
   clearTimeout(refresh);
 
-  if ( globals.state ) {
-    // If a Zoom Room is configured, cache the status and restart the refresh loop;
-    // else just stop checking for Zoom state (do nothing)
-    if ( globals.state.hasOwnProperty('zoom_room') ) {
-      zoomStatus = globals.state.zoom_room
-      refresh = window.setTimeout(getZoomStatus, REFRESH_WAIT);
+  // User feedback: show banner
+  const banner = document.getElementById( 'zoom-room-notification' );
+	banner.querySelector( '.feedback-message' ).innerHTML = 'Joining meeting ' ;
+	// zoomBanner.querySelector( '.meeting-name' ).innerHTML = meetingId ;
+	banner.classList.remove( 'hidden' ) ;
+
+  // bump main content for banner
+
+  // Post join request to orchestrator
+  const payload = JSON.stringify({ zoom_room : { join : { id: meetingId, password: password }}}) ;
+  console.log('payload for zoom join');
+  console.log(payload);
+  updateStatus(payload, () => {
+    getZoomData(); // start the loop again
+
+    // call callback (reset)
+    if (callback) { // Note: Using this pattern so that joinZoomMeeting can be exported for other modules to use in future
+      callback();
     }
-  }
-  // waiting for global state, try again
-  else {
-    refresh = window.setTimeout(getZoomStatus, REFRESH_WAIT);
-  }
+  });
+}
+
+function leaveZoomMeeting(callback=null) {
+  // stop the getZoomData loop
+  clearTimeout(refresh);
+
+  // User feedback: update banner
+  const banner = document.getElementById( 'zoom-room-notification' );
+  banner.querySelector( '.feedback-message' ).innerHTML = 'Leaving meeting ' ;
+  banner.classList.remove( 'hidden' ) ;
+
+  // bump main content for banner
+
+  // Post leave request to orchestrator
+  const payload = JSON.stringify({ zoom_room : { leave : true }}) ;
+  updateStatus(payload, () => {
+    getZoomData(); // start the loop again
+
+    // call callback (reset)
+    if (callback) {
+      callback();
+    }
+  })
 }
 
 function openZoomPrompt() {
-  console.log('openZoomPrompt placeholder');
+  const meetingJoined = zoomData.meeting?.status === "in_meeting" ? true : false ;
 
-  // if no meeting is joined yet, do that dance
-  // else if meeting is joined, leave
-  // document.getElementById('manual-zoom-prompt').classList.toggle('hidden');
+  function handleSuggestedJoinSubmit(e) {
+    // remove the submit listener
+    const modal = document.getElementById('scheduled-zoom-prompt') ;
+    const submitBtn = modal.querySelector('button[name=join-suggested-meeting]') ; // TO DO: refactor to use form/submit?
+    submitBtn.removeEventListener('click', handleSuggestedJoinSubmit);
 
+    // user feedback: style submit button
+    //submitBtn.classList.add(??)
+
+    // callback for updateStatus
+    function reset() {
+      // hide suggested join modal
+      modal.classList.add('hidden');
+
+      // style buttons active
+      //submitBtn.classList.remove(??)
+    }
+
+    joinZoomMeeting( zoomData.suggested_meeting?.meeting_id, zoomData.suggested_meeting?.meeting_password, reset) ;
+  }
+
+  function handleLeaveSubmit(e) {
+    // remove submit listener
+    const modal = document.getElementById('leave-zoom-prompt') ;
+    const submitBtn = modal.querySelector('button[name=leave-meeting]') ; // TO DO: refactor to use form/submit?
+    submitBtn.removeEventListener('click', handleLeaveSubmit);
+
+    // user feedback: style submit button
+
+    function reset() {
+      // hide leave modal
+      modal.classList.add('hidden');
+
+      // style buttons active
+      //submitBtn.classList.remove(??)
+    }
+
+    leaveZoomMeeting(reset);
+  }
+
+  // Open one of three prompts based on Zoom state:
+  //  // Suggested meeting join:
+  if ( !meetingJoined && zoomData.suggested_meeting ) {
+    // update modal text
+    const modal = document.getElementById('scheduled-zoom-prompt');
+    modal.querySelector( '.meeting-name' ).textContent = zoomData.suggested_meeting.meeting_name ;
+
+    // re-attach submit listener
+    modal.querySelector('button[name=join-suggested-meeting]').addEventListener('click', handleSuggestedJoinSubmit);
+
+    openModal(null, 'scheduled-zoom-prompt');
+  }
+  //  // Manual meeting join:
+  else if ( !meetingJoined ) {
+    openManualJoinForm(); // using a global function instead of inline so that Suggested Meeting dismiss can run this routine
+  }
+  //  // Leave meeting prompt
+  else {
+    // re-attach submit listeners etc ...
+    const modal = document.getElementById('leave-zoom-prompt');
+    modal.querySelector('button[name=leave-meeting]').addEventListener('click', handleLeaveSubmit);
+
+    openModal(null, 'leave-zoom-prompt');
+  }
+}
+
+// Making this snippet reusable so that Manual Meeting join can share
+// with Suggested Meeting dismiss
+function openManualJoinForm() {
+  // clear old form input
+  const form = document.getElementById('join-meeting-by-id');
+  form.reset();
+
+  // re-attach form submit listener
+  function handleManualJoinSubmit(e) {
+    e.preventDefault();
+    const modal = document.getElementById('manual-zoom-prompt') ;
+    // const form = e.target;
+    const submitBtn = form.querySelector('button[type=submit]')
+
+    // remove submit listener
+    form.removeEventListener('submit', handleManualJoinSubmit);
+
+    // user feedback: style submit button
+    //submitBtn.classList.add(??)
+
+    // get values from form
+		const meetingId = form.querySelector( "input[name=meeting_id]" ).value ;
+		const password = form.querySelector( "input[name=password]" ).value ;
+
+    // callback for updateStatus
+    function reset() {
+      // hide manual join modal
+      modal.classList.add('hidden');
+
+      // style buttons active
+      //submitBtn.classList.remove(??)
+    }
+
+    joinZoomMeeting( meetingId, password, reset );  
+  }
+  form.addEventListener('submit', handleManualJoinSubmit);
+
+  openModal(null, 'manual-zoom-prompt');
+}
+
+function displayZoomStatus() {
+
+}
+
+function getZoomData() {
+  clearTimeout(refresh);
+
+  if ( globals.state?.zoom_room ) {
+    // Cache Zoom Room status for event listeners
+    zoomData = globals.state.zoom_room ;
+
+    // update the gui to reflect current state, check for abandoned meeting etc.
+    // ...
+
+    refresh = window.setTimeout(getZoomData, REFRESH_WAIT);  
+  }
+  // lost global state, try again (shouldn't get here)
+  else {
+    refresh = window.setTimeout(getZoomData, REFRESH_WAIT);
+  }
 }
 
 function initiateZoomGUI() {
@@ -57,6 +212,8 @@ function initiateZoomGUI() {
   
   // Only need to render Zoom UI components if this system has zoom_room configured
   if ( globals.state.hasOwnProperty("zoom_room") ) {
+    zoomData = globals.state.zoom_room ;
+
     // Add Zoom Room banners to DOM 
     document.getElementById('banners-container').insertAdjacentHTML('beforeend', banners) ;
 
@@ -72,8 +229,10 @@ function initiateZoomGUI() {
       input.addEventListener('click', openZoomPrompt)
     });
 
-    // Attach listeners to inputs in custom Zoom modals
+    // Attach static listeners to inputs in custom Zoom modals, eg. Close/Back buttons
     attachSharedModalListeners();
+    // special handler for the Suggested Meeting prompt dismiss-modal button: Open the manual join prompt
+    document.querySelector('#scheduled-zoom-prompt button.dismiss-modal').addEventListener('click', openManualJoinForm);
 
     // Focus listener for "join meeting" form inputs
     document.querySelectorAll( "form#join_meeting_by_id input" ).forEach( function handler(input) {
@@ -86,6 +245,10 @@ function initiateZoomGUI() {
     // document.querySelectorAll("#sip-toggle button").forEach( function(button){
     //   button.addEventListener( "click", toggleSip );
     // });  
+
+
+    // Start the status loop
+    getZoomData() ;
   } 
 }
 
@@ -93,15 +256,5 @@ function initiateZoomGUI() {
 /* page load listener */
 window.addEventListener("load", async (event) => {
   initiateZoomGUI() ;
-  getZoomStatus() ;
 });
 
-// from get_status:
-// Zoom Room setup
-// if ( response.hasOwnProperty("zoom_room") ) {
-//   captureZoomJoinData( response.zoom_room );
-//   displayZoomStatus( response.zoom_room.sharing_info?.sharing_key, response.zoom_room.current_meeting, response.zoom_room.last_meeting_join_status, response.zoom_room.last_interaction );
-//   checkForAbandonedZoomMeeting( response.zoom_room, response.video ) ;
-//   // checkForMutedZoomRoom( response.zoom_room );
-//   checkForZoomRoomWarnings( response.zoom_room );
-// }

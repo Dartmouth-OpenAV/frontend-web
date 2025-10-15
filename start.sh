922160476c51d1cbf91de2bde3baca483cb5ea55
Jane Reynolds <jane.e.reynolds@dartmouth.edu>
2025-10-15 16:39:13 -0400

    Temporarily reverting 'DEFAULT_ORCHESTRATOR' to 'HOME_ORCHESTRATOR' until deployment gets updated

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
-# Set $DEFAULT_ORCHESTRATOR and $DEFAULT_SYSTEM variables in nginx conf (for /config endpoint)
-envsubst '$DEFAULT_ORCHESTRATOR $DEFAULT_SYSTEM' < /etc/nginx/custom-server.conf > /etc/nginx/conf.d/server.conf
+# Set $HOME_ORCHESTRATOR and $DEFAULT_SYSTEM variables in nginx conf (for /config endpoint)
+envsubst '$HOME_ORCHESTRATOR $DEFAULT_SYSTEM' < /etc/nginx/custom-server.conf > /etc/nginx/conf.d/server.conf
 
 # Launch nginx
 exec nginx -g 'daemon off;'

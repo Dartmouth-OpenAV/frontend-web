c1128d66a888948eed7a8da8e7d0e6ed1540b55c
Isaac Ramirez <isaac.ramirez@dartmouth.edu>
2025-12-02 13:33:12 -0500

    Setting user input to false when updating status from camera automation

--- source/optional_modules/camera_integration/index.js
+++ source/optional_modules/camera_integration/index.js
@@ -55,7 +55,7 @@ function handleZoomSelected(e) {
     const payload = targetBtn
       .getAttribute("data-path")
       .replace(/<value>/, true);
-    updateStatus(payload, null);
+    updateStatus(payload, null, false);
   }
 }
 
@@ -77,7 +77,7 @@ function handlePowerSelected(e) {
     const payload = targetBtn
       .getAttribute("data-path")
       .replace(/<value>/, true);
-    updateStatus(payload, null);
+    updateStatus(payload, null, false);
   }
 }
 
